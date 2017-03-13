7492fb0ca144ac3fbada87e31f4cf648d09ed67d
markel.kalugin <markel.kalugin@gmail.com>
2017-03-13 22:57:10 +0200

    Added controller for home page

---
+++ app/scripts/home/home.controller.js
@@ -0,0 +1,15 @@
+/**
+ * Created by markel on 12.03.17.
+ */
+angular
+    .module('app')
+    .controller('Home.IndexController', Controller);
+
+function Controller() {
+    var vm = this;
+
+    initController();
+
+    function initController() {
+    }
+}
