c76d2ea986c563bb0d3f4b06e1ae51959d780360
markel.kalugin <markel.kalugin@gmail.com>
2017-03-24 16:57:56 +0200

    Changed logic for using user service which provide CRUD functionality.

--- app/scripts/register/register.controller.js
+++ app/scripts/register/register.controller.js
@@ -5,22 +5,41 @@ angular
     .module('app')
     .controller('Register.IndexController', Controller);
 
-function Controller(UserService, $location, $rootScope, FlashService) {
+function Controller(UserService, $location, $rootScope, FlashService, $log) {
     var vm = this;
 
-    vm.register = register;
 
-    function register() {
+    vm.createUser = function() {
         vm.dataLoading = true;
-        UserService.Create(vm.user)
-            .then(function (response) {
-                if (response.success) {
-                    FlashService.Success('Registration successful', true);
-                    $location.path('/login');
-                } else {
-                    FlashService.Error(response.message);
-                    vm.dataLoading = false;
-                }
-            });
-    }
+        UserService.create(vm.user, function(data) {
+            if (data['status'] === 'OK') {
+                FlashService.Success('Registration successful', true);
+                $location.path('/login');
+            } else {
+                FlashService.Error(data['error_message']);
+                vm.dataLoading = false;
+            }
+        });
+    };
+
+    // Get a user
+    vm.getUser = function() {
+        UserService.get({username: vm.user.username}, function(data) {
+           // do something which you want with response
+        });
+    };
+
+    // Update a user
+    vm.updateUser = function() {
+        UserService.update(vm.user, function(data) {
+            // do something which you want with response
+       });
+    };
+
+    // Delete a user
+    vm.deleteUser = function() {
+        UserService.remove({username: vm.user.username}, function(data) {
+         // do something which you want with response
+        });
+    };
 }
