10d2f99ce8936e039fbf258b01143ace3c5cb50a
markel.kalugin <markel.kalugin@gmail.com>
2017-03-25 05:03:06 +0200

    Changed flag isArray in query method in user service

--- app/scripts/app-services/user.service.js
+++ app/scripts/app-services/user.service.js
@@ -6,7 +6,7 @@ angular.module('app')
 
 function Service($resource) {
     return $resource('https://hopstertest-161207.appspot.com/api/v1/users', {}, {
-        query: { method: "GET", isArray: true },
+        query: { method: "GET", isArray: false },
         create: { method: "POST"},
         get: { method: "GET"},
         remove: { method: "DELETE"},
